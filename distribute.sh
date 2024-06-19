216dce6aa5d537e515e69427bc4066fa5820ec61
Konstantin Protzen <code@kondev.de>
2024-06-19 18:12:10 +0200

    Add new distribution script

--- distribute.sh
+++ distribute.sh
@@ -6,26 +6,29 @@ mkdir ./dist
 mkdir ./dist/ABs
 mkdir ./dist/HTML
 
+# Vorher-Pfad speichern
+START_PWD=$(pwd)
+
 # Funktion um .tex Dateien zu verarbeiten
 process_tex_files() {
   local dir="$1"
   for tex_file in "$dir"/*.tex; do
-    if [[ -f "$tex_file" ]]; then
+    if [[ -e "$tex_file" ]]; then
       local parent_dir
       parent_dir=$(basename "$(dirname "$dir")")
-      pdflatex -shell-escape -output-directory "$dir" -halt-on-error "$tex_file" | grep '^!.*' -A200 --color=always 
-      pdf_file="${tex_file%.tex}.pdf"
-      if [[ -f "$pdf_file" ]]; then
-        mv "$pdf_file" "./dist/ABs/${parent_dir}.pdf"
-      fi
+      cd "$dir"
+      for tf in ./*.tex; do
+        pdflatex -shell-escape -halt-on-error "$tf" | grep '^!.*' -A200 --color=always
+        cp "${tf%.tex}.pdf" "$START_PWD/dist/ABs/${parent_dir}.pdf"
+      done
+      cd "$START_PWD"
     fi
   done
 }
-
 # Ordner ./ABs und Unterordner nach .tex Dateien durchsuchen
 find ./ABs -type d -name 'AB' | while read -r subdir; do
   process_tex_files "$subdir"
 done
 
-# Minted temporäre Ordner löschen
-rm -r _minted*
+# HTML Dateien kopieren
+find ./ABs/*/*.html -exec cp {} "./dist/HTML" \;
